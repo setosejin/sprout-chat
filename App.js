9205b04971f637b16ae0649d24c8c2413107acf6
Sejin Kim <lemon05090@gmail.com>
2020-10-19 15:10:15 +0900

    Add callback function

--- App.js
+++ App.js
@@ -2,6 +2,10 @@
 var express = require('express'),
     port = process.env.PORT || 3000,
     app = express(),
+    client_id = '8HkITidEmr1tQaw5jtAL',
+    client_secret = 'UEoWMdGYuq',
+    state = "RAMDOM_STATE",
+    redirectURI = encodeURI("http://localhost:3000/chat"),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server), // socket.io 를 사용하기 위한 io 객체 생성
     users = {
@@ -12,6 +16,7 @@ var express = require('express'),
     }, // 기본 회원이 담기는 object
     onlineUsers = {}; // 현재 online인 회원이 담기는 object
     
+
 app.use(express.static('public')); // 정적파일(css, js...)을 사용하기 위한 path 지정
 
 app.get('/', function (req, res) {
@@ -25,6 +30,29 @@ app.get('/login', function (req, res) {
 app.get('/chat', function (req, res) {
     res.sendFile(__dirname + '/chat.html');
 }); // '/chat'으로 들어오는 요청은 chat.html 을 렌더링
+
+
+app.get('/callback', function (req, res) {
+    code = req.query.code;
+    state = req.query.state;
+    api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
+      + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
+    var request = require('request');
+    var options = {
+        url: api_url,
+        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+      };
+    request.get(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+        res.end(body);
+      } else {
+        res.status(response.statusCode).end();
+        console.log('error = ' + response.statusCode);
+      }
+    });
+});
+
 
 server.listen(port, () => {
     console.log(`server open ${port}`);
